d9e05e993dda1c6856f671669006139647b9c2ea
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-05-06 13:50:40 -0300

    Implementing custom exceptions

--- controllers/loadAllEmployees.php
+++ controllers/loadAllEmployees.php
@@ -1,12 +1,13 @@
 <?php
     require_once("../model/ConnectionSingleton.php");
     require_once("../model/EmployeesDAO.php");
-
     $con = ConnectionSingleton::getInstace()->getPDOConnection();
 
-    $em = new EmployessDAO($con);
-
-    $result = $em->loadAll();
-    
+    try{
+        $em = new EmployessDAO($con);
+        $result = $em->loadAll($_POST['values']=10);
+    }catch(CrudException $e){
+        echo $e->getMessage();
+    }
     echo json_encode($result);
 ?>
--- model/EmployeesDAO.php
+++ model/EmployeesDAO.php
@@ -1,18 +1,31 @@
 <?php
     require_once("Employee.php");
-    
+    require_once("CrudException.php");
+
     class EmployessDAO{
         private $connection = null;
+        private $stmt = null;
+        private $sql = null;
 
         public function __construct(PDO $connection)
         {
             $this->connection = $connection;    
         }
-
-        public function loadAll(){
-            $sql = "select first_name, last_name, email, gender, job_title from employees where id< 10";
-            $result = $this->connection->query($sql)->fetchAll((PDO::FETCH_ASSOC));
-            return $result;
+        
+        public function loadAll(int $qtd){
+            try{
+                $quantity = $qtd;
+                $this->sql = "select first_name, last_name, email, gender, job_title from employees where id<:qtd";
+                $this->stmt = $this->connection->prepare($this->sql);
+                $this->stmt->bindValue(":qtd", $quantity);
+                $this->stmt->execute();
+                $result = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $result;
+            }catch(PDOException $e){
+                throw new CrudException("Error CrudException: " . $e->getMessage()); 
+            }
         }
+
+
     }
 ?>
